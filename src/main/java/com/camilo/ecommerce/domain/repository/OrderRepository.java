fa295f9d639e0c897ae423a08be95f81e0e1c933
Cristian Carvajal <camilocc597@gmail.com>
2025-10-26 22:40:27 -0500

    domain classes

--- src/main/java/com/camilo/ecommerce/domain/repository/OrderRepository.java
+++
@@ -1,4 +0,0 @@
-package com.camilo.ecommerce.domain.repository;
-
-public class OrderRepository {
-}
--- src/main/java/com/camilo/ecommerce/domain/repository/UserRepository.java
+++ src/main/java/com/camilo/ecommerce/domain/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.camilo.ecommerce.domain.repository;
 
-public class UserRepository {
+import com.camilo.ecommerce.domain.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+    List<User> findAll();
+    Optional<User> findById(String email);
+    User save(User user);
+    void deleteById(String email);
+    boolean existsById(String email);
 }
